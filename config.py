e4586d4ef67461ab952eca223f44766d9c1ac192
cyberIntruderZ <hamzamurtala306@gmail.com>
2026-03-08 04:37:05 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# Demo configuration file
+# Created for OSINT credential scanning demonstration
+
+username = "demo_user"
+password = "demo_password123"
+
+database_host = "localhost"
+database_port = 3306
+
+api_url = "https://api.example.com"
